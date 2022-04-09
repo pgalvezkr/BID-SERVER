158466c1c9b63323b321e6e07dc3e67c44dac9bd
Estefania Galvez <egalvez@cobiscorp.com>
2022-04-09 12:55:01 -0500

    First version
    First version

---
+++ bid-server/src/main/java/com/witbooking/bidserver/BidServerApplication.java
@@ -0,0 +1,13 @@
+package com.witbooking.bidserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidServerApplication.class, args);
+	}
+
+}
---
+++ bid-server/src/test/java/com/witbooking/bidserver/BidServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.witbooking.bidserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
